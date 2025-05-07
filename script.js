39a65b6b2a5a02a05847381eb27e26edbfab6be6
jonas <147396302+jonasbrik@users.noreply.github.com>
2025-05-07 11:41:55 +0000

    a8-3

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-    const alternaContraste = document.getElementById('alterna-contraste');
-
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+ 
     botaoDeAcessibilidade.addEventListener('click', function (){
-     botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
      opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
  
      const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
@@ -15,18 +14,26 @@ document.addEventListener('DOMContentLoaded', function(){
      const aumentaFonteBotao = document.getElementById('aumentar-fonte');
      const diminuiFonteBotao = document.getElementById('diminuir-fonte');
      
+     const alternaContraste = document.getElementById('alterna-contraste')
  
      let tamanhoAtualFonte = 1;
  
      aumentaFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte += 0.1;
-         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+         if(tamanhoAtualFonte < 5,0 ){
+            tamanhoAtualFonte += 0.1;
+             document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+         }
+         
  
      })
  
      diminuiFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte -= 0.1;
-         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+         if(tamanhoAtualFonte > 0,6){
+            tamanhoAtualFonte-= 0,1;
+             document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+         }
+
+        
  
      })
  
